59a55117ea4b33965242eb9ecbab899c98af443c
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2014-11-05 15:21:40 +0100

    fix2

--- db/migrate/20141104132210_add_initial_users.rb
+++ db/migrate/20141104132210_add_initial_users.rb
@@ -1,10 +1,8 @@
 class AddInitialUsers < ActiveRecord::Migration
   def up
-	role_guest = Role.find_by_name("guest")
-	role_admin = Role.find_by_name("admin")
 	User.reset_column_information
-	User.create(:role_id => role_guest.id, :username => "Guest", :name => "guest")
-	User.create(:role_id => role_admin.id, :username => "admin", :password => "$1$c9c6b4ce$00e46beea797471c7a929694da5bf83d", :name => "Administrator")
+	User.create(:role_id => 1, :username => "Guest", :name => "guest")
+	User.create(:role_id => 3, :username => "admin", :password => "$1$c9c6b4ce$00e46beea797471c7a929694da5bf83d", :name => "Administrator")
 
   end
 
